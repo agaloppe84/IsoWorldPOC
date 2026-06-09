0e85d11b19c6ff5ec63ee06009d68f3b08b06f8e
GALOPPE aurélien <agaloppe@me.com>
2026-06-09 21:56:23 +0200

    Connect EngineCore to Xcode app

--- IsoWorldPOC/IsoWorldPOC/ContentView.swift
+++ IsoWorldPOC/IsoWorldPOC/ContentView.swift
@@ -6,19 +6,10 @@
 //
 
 import SwiftUI
+import EngineCore
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("EngineCore OK")
     }
-}
-
-#Preview {
-    ContentView()
 }
